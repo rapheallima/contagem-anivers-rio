17c11c44da4265d8182b4575fb215dbed20a2eae
Raphael Lima <raphaellima365@gmail.com>
2024-03-23 01:07:47 -0300

    criando site de contagem regressiva para aniversário

---
+++ src/scripts/main.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+const dataEvento = new Date("Nov 13, 2024 18:00:00")
+const timeStampEvento = dataEvento.getTime()
+
+const contaHoras = setInterval(function () {
+    const agora = new Date()
+    const timeStampAtual = agora.getTime()
+
+    const distanciaEvento = timeStampEvento - timeStampAtual;
+
+    const diaMs = 1000 * 60 * 60 * 24;
+    const horaMs = 1000 * 60 * 60;
+    const minutoMS = 1000 * 60;
+
+    const diasEvento = Math.floor(distanciaEvento / diaMs);
+    const horasEvento = Math.floor((distanciaEvento % diaMs) / horaMs);
+    const minutosEvento = Math.floor((distanciaEvento % horaMs) / minutoMS);
+    const segundosEvento = Math.floor((distanciaEvento % minutoMS) / 1000)
+
+    console.log(diasEvento);
+    console.log(horasEvento);
+    console.log(minutosEvento);
+    console.log(segundosEvento);
+
+    document.getElementById('contador').innerHTML = `${diasEvento}d ${horasEvento}h ${minutosEvento}m ${segundosEvento}s`
+
+    if (distanciaEvento < 0) {
+        clearInterval(contaHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+
+}, 1000)
